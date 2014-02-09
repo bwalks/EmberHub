597c4d603f8bd796e19d979964b9949fe3e123f7
Brendan Walker <brendan.walker4@gmail.com>
2014-02-09 15:25:44 -0800

    Adding logo and build info

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+  			options: {
+    			separator: '\n'
+  			},
+  			dist: {
+    			src: ['js/*.js'],
+    		 dest: 'dist/<%= pkg.name %>.js'
+  			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTask('default', ['concat']);
+};
